511cc1693be625ada7afb7384d6c8313eb5d942b
garwey@126.com <garwey@126.com>
2017-07-06 21:07:31 +0800

    first.

---
+++ src/oa/test/TestAction.java
@@ -0,0 +1,11 @@
+package oa.test;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport {
+	@Override
+	public String execute() throws Exception {
+		System.out.println("TestAction.execute()");
+		return "success";
+	}
+}
